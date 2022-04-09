de3de6ea07a2132d884462f8b3eb97c005b496ac
Manchere <manucheremeh1995@gmail.com>
2022-04-09 15:29:57 +0200

    WIP: json object in scene system

--- perso.py
+++ perso.py
@@ -1,6 +1,13 @@
 import maya.cmds as cmds
 import json
 
+jpath = cmds.internalVar(usd=True) + 'TP_Perso/'
+
+# reading json
+with open(jpath + 'data.json') as jsonFile:
+    jsonObject = json.load(jsonFile)
+
+
 class Skeleton:
 
     def __init__(self, data):
@@ -16,7 +23,7 @@ class Skeleton:
     def constructPartByPos(self, part):
         for j_name, j_info in self.Skel[part]['joints'].items():
             x, y, z = (i for i in j_info['pos_info'])
-            cmds.joint(n=str(joint_name), p=(x, y, z))
+            cmds.joint(n=str(j_name), p=(x, y, z))
             cmds.select(d=True)
 
     def constructPartByClust(self):
@@ -42,13 +49,7 @@ class Skeleton:
 
     def skeletonize(self):
         pass
-
-
-jpath = cmds.internalVar(usd=True) + 'TP_Perso/'
-
-# reading json
-with open(jpath + 'data.json') as jsonFile:
-    jsonObject = json.load(jsonFile)
+        # self.constructPartByPos()
 
 
 class Perso:
@@ -56,7 +57,10 @@ class Perso:
     def __init__(self, data):
         self.Data = data
         self.Path = cmds.internalVar(usd=True) + 'TP_Perso/Partie/'
-        self.Current = self.Data['initialization']
+        self.Current = self.Data['obj_init']
+        self.Selection = self.Data['selection_init']
+        self.Skeleton = Skeleton(data)
+
         self.__checkCurrent()
 
         self.__addJointObj('neck', 0, 3.211, 0.161)
@@ -64,14 +68,51 @@ class Perso:
         self.__addJointObj('l_ankle', 0.372, 0.9, 0.121)
         self.__initAddPart('deca_body')
 
+        print('check scene')
+        print(self.checkObjInSceneAsset())
+
+        print('current')
+        print(self.Current.values())
+
     def __addJointObj(self, jointName, xPos, yPos, rd):
         if not cmds.objExists(jointName):
             cmds.polySphere(n=jointName, sx=15, sy=15, r=rd)
             cmds.move(xPos, yPos, xy=True)
             # self.__lockTransform(jointName)
 
+    def __checkCurrent(self):
+        for k, v in self.Current.items():
+            if not cmds.objExists(str()):
+                self.Current[k] = self.Selection[k] = None
+            else:
+                self.Current[k] = v
+                self.Selection[k] = self.getSelectionNameFromAsset(v)
+
+    def __lockTransform(self, objName):
+        for attr in self.Data['obj_attributes']:
+            cmds.setAttr(objName + '.' + str(self.Data['obj_attributes'][attr]['attribute_name']),
+                         lock=self.Data['obj_attributes'][attr]['locked'])
+
     def __initAddPart(self, partName):
         pass
+
+    def __intersection(self, lst1, lst2):
+        temp = set(lst2)
+        lst3 = [value for value in lst1 if value in temp]
+        return lst3
+
+    def checkObjInSceneAsset(self):
+        info = [s for v in self.Data['elements'].values() for j, s in v.items() if j == 'b_asset_name' or j == 'type']
+        grp_info = zip(*[iter(info)] * 2)
+        obj_in_scene = cmds.ls(type='transform')
+
+        for single_tuple in grp_info:
+            if single_tuple[0] in obj_in_scene:
+                print('single_tuple')
+                print(single_tuple[0])
+                grp_info.remove(single_tuple)
+        return grp_info
+
 
     def addPart(self, name):
         self.__checkCurrent()
@@ -85,22 +126,37 @@ class Perso:
             cmds.file(self.Path + asset + '.obj', i=True, lck=True, gr=isGrouped, gn=asset)
 
             self.Current[partType] = asset
-            #self.__lockTransform(asset)
+            self.Selection[partType] = menuName
+            # self.__lockTransform(asset)
 
-    def __checkCurrent(self):
-        for k, v in self.Current.items():
-            if not cmds.objExists(str(v)):
-                print(k, v)
-                self.Current[k] = None
+    # find dropdown name when given asset name
+    def getSelectionNameFromAsset(self, asset):
+        return [k for k, v in self.Data['elements'].items() if ('b_asset_name', asset) in v.items()]
 
-    def __lockTransform(self, objName):
-        for attr in self.Data['obj_attributes']:
-            cmds.setAttr(objName + '.' + str(self.Data['obj_attributes'][attr]['attribute_name']),
-                         lock=self.Data['obj_attributes'][attr]['locked'])
-
-    def applySymmetry(self, ):
+    def applySymmetry(self, ok):
         if cmds.checkBox('sym', q=True, v=True):
             pass
+
+    def __constructFullPerso(self):
+        for part in self.Current.values():
+            if part is not None:
+                self.Skeleton.constructPartByPos(part)
+
+    def __createJointChainFullPerso(self):
+        for part in self.Current.values():
+            print(part)
+            if part is not None:
+                self.Skeleton.createJointChain(part)
+
+    def __attachJointFullPerso(self):
+        for part in self.Current.values():
+            if part is not None:
+                self.Skeleton.attachJoint(part)
+
+    def skeletonize(self):
+        self.__constructFullPerso()
+        self.__createJointChainFullPerso()
+        self.__attachJointFullPerso()
 
 
 class Texturing:
@@ -130,7 +186,6 @@ def make_optmenu(optMenName, optMenLbl, menuItems):
     for item in menuItems:
         cmds.menuItem(item)
 
-
 def colorCgBtn(btnName, btnLbl, cmd):
     cmds.button(btnName, label=btnLbl, command=cmd)
 
@@ -138,149 +193,165 @@ def colorCgBtn(btnName, btnLbl, cmd):
 if cmds.window('Perso_Maker', exists=True):
     cmds.deleteUI('Perso_Maker')
 
-cmds.window('Perso_Maker', title='Perso Maker')
-cmds.frameLayout(label='Body Part Modification', cll=False, cl=False, bgc=[0.2, .35, 0.2], w=200)
+cmds.window('Perso_Maker', title='Custom Crowd', w=400)
+tabs = cmds.tabLayout(innerMarginWidth=5, innerMarginHeight=5)
 
+t = cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.checkBox('sym', label='Apply Symmetry')
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+cmds.checkBox('syme', label='Allow Lost Parts')
 
 pers = Perso(jsonObject)
 tex = Texturing()
 
 # Bouton head
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Head', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Head', 'Head Type', ['box', 'sauce pan', 'bowl', 'lamp'])
 cmds.optionMenu('Head', cc=lambda x: pers.addPart('Head'), e=True)
 colorCgBtn('head_color', 'Change head color', lambda x: tex.setTexture(pers.Current['head']))
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Bouton Body
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Body', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Body', 'Body Type', ['decagon body', 'wavy body'])
 cmds.optionMenu('Body', cc=lambda x: pers.addPart('Body'), e=True)
 colorCgBtn('body_color', 'Change body color', lambda x: tex.setTexture(pers.Current['body']))
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Bouton Left-Hand
-
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Left Hand', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Left_hand', 'Left Hand Type', ['left boxing glove', 'left mecha hand'])
 colorCgBtn('l_hand_color', 'Change hand color', lambda x: tex.setTexture(pers.Current['left_hand']))
 cmds.optionMenu('Left_hand', cc=lambda x: pers.addPart('Left_hand'), e=True)
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Bouton Right-Hand
-
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Right Hand', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Right_hand', 'Right Hand Type', ['right boxing glove', 'right mecha hand'])
 colorCgBtn('r_hand_color', 'Change hand color', lambda x: tex.setTexture(pers.Current['right_hand']))
 cmds.optionMenu('Right_hand', cc=lambda x: pers.addPart('Right_hand'), e=True)
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Bouton Left-Arm
-
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Left Arm', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Left_arm', 'Left Arm Type', ['left mecha arm', 'smooth surface'])
 cmds.optionMenu('Left_arm', cc=lambda x: pers.addPart('Left_arm'), e=True)
 colorCgBtn('l_arm_color', 'Change arm color', lambda x: tex.setTexture(pers.Current['left_arm']))
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Bouton Right-Arm
-
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Right Arm', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Right_arm', 'Right Arm Type', ['right mecha arm', 'smooth surface'])
 cmds.optionMenu('Right_arm', cc=lambda x: pers.addPart('Right_arm'), e=True)
 colorCgBtn('r_arm_color', 'Change arm color', lambda x: tex.setTexture(pers.Current['right_arm']))
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Bouton Left-Leg
-
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Left Leg', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Left_leg', 'Left Leg Type', ['left mecha leg', 'smooth surface'])
 cmds.optionMenu('Left_leg', cc=lambda x: pers.addPart('Left_leg'), e=True)
 colorCgBtn('l_leg_color', 'Change head color', lambda x: tex.setTexture(pers.Current['left_leg']))
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Bouton Right-Leg
-
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Right leg', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Right_leg', 'Right Leg Type', ['right mecha leg', 'smooth surface'])
 cmds.optionMenu('Right_leg', cc=lambda x: pers.addPart('Right_leg'), e=True)
 colorCgBtn('r_leg_color', 'Change leg color', lambda x: tex.setTexture(pers.Current['right_leg']))
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Bouton Left-Foot
-
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Left foot', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Left_foot', 'Left Foot Type', ['left boot', 'left shoe'])
 cmds.optionMenu('Left_foot', cc=lambda x: pers.addPart('Left_foot'), e=True)
 colorCgBtn('l_foot_color', 'Change foot color', lambda x: tex.setTexture(pers.Current['left_foot']))
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Bouton Right-Foot
-
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Right Foot', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Right_foot', 'Right Foot Type', ['right boot', 'right shoe'])
 cmds.optionMenu('Right_foot', cc=lambda x: pers.addPart('Right_foot'), e=True)
 colorCgBtn('r_foot_color', 'Change foot color', lambda x: tex.setTexture(pers.Current['right_foot']))
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Left Back
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Left Back', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Left_back', 'Left Back Type', ['left wing', 'left stick'])
 cmds.optionMenu('Left_back', cc=lambda x: pers.addPart('Left_back'), e=True)
 colorCgBtn('l_back_color', 'Change back object color', lambda x: tex.setTexture(pers.Current['left_back']))
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Right Back
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Right Back', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Right_back', 'Right Back Type', ['right wing', 'right stick'])
 cmds.optionMenu('Right_back', cc=lambda x: pers.addPart('Right_back'), e=True)
 colorCgBtn('r_back_color', 'Change back object color', lambda x: tex.setTexture(pers.Current['right_back']))
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Eyes custom
-
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Eyes', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Eyes', 'Eyes Type', ['boot', 'shoe'])
 cmds.optionMenu('Eyes', cc=lambda x: pers.addPart('Eyes'), e=True)
 colorCgBtn('eyes_color', 'Change eyes color', lambda x: tex.setTexture(pers.Current['eyes']))
-cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+
 cmds.setParent('..')
 cmds.setParent('..')
 
 # Tail custom
-
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
 cmds.frameLayout(label='Tail', cll=True, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
 make_optmenu('Tail', 'Tail Type', ['mecha tail', 'bionic'])
 cmds.optionMenu('Tail', cc=lambda x: pers.addPart('Tail'), e=True)
+colorCgBtn('tail_color', 'Change tail color', lambda x: tex.setTexture(pers.Current['tail']))
+cmds.setParent('..')
+cmds.setParent('..')
+
 cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+cmds.frameLayout(label='Create', cll=False, cl=True, bgc=[0.2, 0.2, 0.2], w=200)
+cmds.button(label="Finialize Character", bgc=[0, 0.2, 0.3], command='pers.skeletonize()', w=200)
+cmds.button(label='Randomize Character', bgc=[0, 0.2, 0], command='', w=200)
+
 cmds.setParent('..')
 cmds.setParent('..')
+
+cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=2)
+cmds.frameLayout(label='', cll=False, cl=True, bgc=[0.2, 0.2, 0.8], w=200)
 
 cmds.setParent('..')
 cmds.setParent('..')
@@ -288,14 +359,15 @@ cmds.setParent('..')
 
 # ----------------------------------------------------------------------
 
-# Skeleton Definitioon
+# Skeleton Definition
 
-cmds.frameLayout(label='Skeleton Definition', cll=False, cl=False, bgc=[0.2, .35, 0.2], w=200)
+u = cmds.frameLayout(label='Crowd Definition', cll=False, cl=False, bgc=[0.1, .35, 0.], w=200)
 cmds.rowColumnLayout(rowSpacing=(20, 20), numberOfColumns=1)
-
-cmds.button(label='Create FK', bgc=[0.4, 0.4, 0.4], command='', w=400)
-cmds.button(label='Create IK', bgc=[0, 0, .4], command='')
+cmds.button()
+cmds.setParent('..')
+cmds.setParent('..')
 # ----------------------------------------------------------------------------------------------
+cmds.tabLayout(tabs, edit=True, tabLabel=((t, "Character"), (u, "Crowd")))
 # On affiche la fenetre
 cmds.showWindow()
 jsonFile.close()
